3172c57b4871eacd451ac2f6a2af11930e8353a2
Martin Strambach <martin.strambach@livesport.eu>
2025-11-19 00:56:12 +0100

    Initial Commit

---
+++ BridgeCommander/BridgeCommanderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BridgeCommanderApp.swift
+//  BridgeCommander
+//
+//  Created by Martin Štrambach on 19.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct BridgeCommanderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BridgeCommander/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BridgeCommander
+//
+//  Created by Martin Štrambach on 19.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
